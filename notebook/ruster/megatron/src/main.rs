762b34cf870b09b4baf34b604ce634d8664f0ec9
屈文斌 <quwenbin@blchina.com>
2019-08-10 23:10:06 +0800

    rust

--- notebook/ruster/megatron/src/main.rs
+++ notebook/ruster/megatron/src/main.rs
@@ -1,3 +1,15 @@
+mod learn {
+    pub trait Summarizable{
+        fn summary(&self) -> String
+    }
+}
+
+mod learn;
+
+pub struct NewsArticle{
+    
+}
+
 fn main() {
     println!("Hello, world!");
 }
